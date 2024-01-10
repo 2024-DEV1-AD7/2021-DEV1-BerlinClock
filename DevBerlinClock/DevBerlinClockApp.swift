16b8e177a27f7390ff83dc7ecdb536e99064d907
2024-DEV1-AD7 <alidhanany@gmail.com>
2024-01-10 20:42:43 +0100

    Project Init

---
+++ DevBerlinClock/DevBerlinClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevBerlinClockApp.swift
+//  DevBerlinClock
+//
+//  Created by Ali Dhanani on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct DevBerlinClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
