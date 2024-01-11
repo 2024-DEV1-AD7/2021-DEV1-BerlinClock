f41b8ecfb28c93ecdf0baffb8af36ea7b0ef2658
2024-DEV1-AD7 <alidhanany@gmail.com>
2024-01-11 21:46:09 +0100

    Refactor Design fix

--- DevBerlinClock/View/MinutesRowView.swift
+++ DevBerlinClock/View/MinutesRowView.swift
@@ -14,27 +14,71 @@ struct MinutesRowView: View {
     var body: some View {
         HStack {
             if isTopRow {
-                topRow()
+                ForEach(0..<11) { index in
+                    Group {
+                        if index == 0 {
+                            UnevenRoundedRectangle(cornerRadii: .init(topLeading: 10.0, bottomLeading: 10))
+                                .frame(width: 20, height: 30)
+                                .foregroundColor(MinutesHelper.shared.lightOnMinutesRowBy(lightCount: self.lightsCount, atIndex: index))
+                                .overlay(
+                                    UnevenRoundedRectangle(cornerRadii: .init(topLeading: 10.0, bottomLeading: 10))
+                                        .stroke(Color.black, lineWidth: 1)
+                                )
+                        } else if index == 10 {
+                            UnevenRoundedRectangle(cornerRadii: .init(bottomTrailing: 10, topTrailing: 10))
+                                .frame(width: 20, height: 30)
+                                .foregroundColor(MinutesHelper.shared.lightOnMinutesRowBy(lightCount: self.lightsCount, atIndex: index))
+                                .overlay(
+                                    UnevenRoundedRectangle(cornerRadii: .init(bottomTrailing: 10, topTrailing: 10))
+                                        .stroke(Color.black, lineWidth: 1)
+                                )
+                        } else {
+                            Rectangle()
+                                .frame(width: 20, height: 30)
+                                .foregroundColor(MinutesHelper.shared.lightOnMinutesRowBy(lightCount: self.lightsCount, atIndex: index))
+                                .cornerRadius(isTopRow ? 0 : 3)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: isTopRow ? 0 : 3)
+                                        .stroke(Color.black, lineWidth: 1)
+                                )
+                        }
+                    }
+                }
             } else {
-                bottomRow()
+                ForEach(0..<4) { index in
+                    Group {
+                        if index == 0 {
+                            UnevenRoundedRectangle(cornerRadii: .init(topLeading: 10.0, bottomLeading: 10))
+                                .foregroundColor(MinutesHelper.shared.checkColorForMinutes(By: index < self.lightsCount))
+                                .frame(width: 70, height: 30)
+                                .overlay(
+                                    UnevenRoundedRectangle(cornerRadii: .init(topLeading: 10.0, bottomLeading: 10))
+                                        .stroke(Color.black, lineWidth: 1)
+                                )
+                        } else if index == 1 || index == 2 {
+                            Rectangle()
+                                .frame(width: 70, height: 30)
+                                .foregroundColor(MinutesHelper.shared.checkColorForMinutes(By: index < self.lightsCount))
+                                .cornerRadius(isTopRow ? 0 : 3)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 0)
+                                        .stroke(Color.black, lineWidth: 1)
+                                )
+                        } else if index == 3 {
+                            UnevenRoundedRectangle(cornerRadii: .init(bottomTrailing: 10, topTrailing: 10))
+                                .foregroundColor(MinutesHelper.shared.checkColorForMinutes(By: index < self.lightsCount))
+                                .frame(width: 70, height: 30)
+                                .overlay(
+                                    UnevenRoundedRectangle(cornerRadii: .init(bottomTrailing: 10, topTrailing: 10))
+                                        .stroke(Color.black, lineWidth: 1)
+                                )
+                        }
+                    }
+                }
             }
         }
     }
-
-    private func topRow() -> some View {
-        ForEach(0..<11) { index in
-            GlobalHelper.shared.RoundedRectangleWithStroke(index: index, lightsCount: lightsCount, isTopRow: isTopRow)
-        }
-    }
-
-    private func bottomRow() -> some View {
-        ForEach(0..<4) { index in
-            GlobalHelper.shared.RoundedRectangleWithStroke(index: index, lightsCount: lightsCount, isTopRow: isTopRow, isBottomRow: true)
-                .frame(width: 70, height: 30)
-        }
-    }
 }
-
 
 //#if DEBUG
 //struct MinutesRowView_Previews: PreviewProvider {
